d95593fb4f535c5d751b8a221e7724ddbdcad013
shahmem <kpmshameemudheenkp@gmail.com>
2024-11-20 11:42:36 +0530

    remove centered  array

---
+++ Arrays/remv-center-array.js
@@ -0,0 +1,6 @@
+const myarray =[1,2,3,4,5,6];
+for (let i=0; i< myarray.length;i++)
+    {       let k=(myarray.length-1)
+            if (i != k/2) 
+        console.log(myarray[i]);    
+        }
