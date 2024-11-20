8ef52dce270d95417df6fc325d9632e65e496378
shahmem <kpmshameemudheenkp@gmail.com>
2024-11-20 11:43:58 +0530

    remove last array

---
+++ Arrays/remv-last-array.js
@@ -0,0 +1,18 @@
+// const myarray =["car","bike","lory","plane"];
+// for (let i=0; i< myarray.length-1;i++){
+//     console.log(myarray[i]);
+// }
+
+
+// const fruit=["mango","apple","banana"]
+// // var a="kiwi"
+// fruit[0]="kiwi"
+// console.log(fruit);
+
+
+
+// const fruit=["mango","apple","banana"]
+// for(let i=0;i<=fruit;i++){
+        
+
+// }
