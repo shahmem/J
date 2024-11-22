72ca4c9e916907991e469b2e4b1c799a79c21ad7
shahmem <kpmshameemudheenkp@gmail.com>
2024-11-22 18:29:48 +0530

    helo world

---
+++ Functions/function1.js
@@ -0,0 +1,6 @@
+//Write a JavaScript function to print the “Hello World” message?
+// --------------------------------------------------------------
+function demo() {
+	console.log("Helo World");
+}
+demo();
