f2beac3a1c0452c7958771464e4daf0987d92ffe
shahmem <kpmshameemudheenkp@gmail.com>
2024-11-22 19:41:51 +0530

    parameter

---
+++ Functions/function2.js
@@ -0,0 +1,9 @@
+// Write a function that returns the square of a number
+// --------------------------------------------------------------
+function demo(a) {
+
+	return a ** 2
+
+}
+console.log(demo(4));
+
