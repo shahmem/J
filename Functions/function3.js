07e87ebd1cf99df3984e07ff959f7ef6f12ae370
shahmem <kpmshameemudheenkp@gmail.com>
2024-11-26 15:02:51 +0530

    temprature

---
+++ Functions/function3.js
@@ -0,0 +1,6 @@
+//Write a function to convert Celsius to Fahrenheit?
+// --------------------------------------------------------------
+function temprature(Celsius) {
+	console.log(Celsius * 9/5 + 32);
+}
+temprature(1);
