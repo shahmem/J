93cd2831cb53362bd53692ce2b63f323ff36890e
shahmem <kpmshameemudheenkp@gmail.com>
2024-11-26 15:03:20 +0530

    area

---
+++ Functions/function4.js
@@ -0,0 +1,6 @@
+//Write a function to find the area of a given Rectangle?
+// --------------------------------------------------------------
+function area(width , length) {
+	console.log(width * length);
+}
+area(4,6);
