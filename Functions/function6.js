e9a76e6ef6617b9cc687280e64550dc8514d4fee
shahmem <kpmshameemudheenkp@gmail.com>
2024-11-27 11:05:37 +0530

    random number

---
+++ Functions/function6.js
@@ -0,0 +1,8 @@
+
+// Write a function to generate a random number?
+//--------------------------------------------
+function getrandomnumber() {
+    return Math.random()*100
+}
+result=getrandomnumber();
+console.log(result);
